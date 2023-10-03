063c132a39673bdf443ecefe445b3bd56d4647cb
andrewg <andrew.giannopoulos@gmail.com>
2023-10-03 22:08:26 +1100

    more test stuff

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import seaborn as sns
 import numpy as np
 from scipy.stats import norm
 from scipy import stats
+from sklearn.preprocessing import StandardScaler
 
 df_train = pd.read_csv('train.csv')
 
@@ -14,19 +15,19 @@ print(df_train['SalePrice'].describe())
 
 sns.distplot(df_train['SalePrice'])
 
-plt.show()
+# plt.show()
 
 var = 'GrLivArea'
 data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
 data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000))
 
-plt.show()
+# plt.show()
 
 var = 'TotalBsmtSF'
 data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
 data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000))
 
-plt.show()
+# plt.show()
 
 #box plot overallqual/saleprice
 var = 'OverallQual'
@@ -35,4 +36,56 @@ f, ax = plt.subplots(figsize=(8, 6))
 fig = sns.boxplot(x=var, y="SalePrice", data=data)
 fig.axis(ymin=0, ymax=800000)
 
-plt.show()
+# plt.show()
+
+var = 'YearBuilt'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+f, ax = plt.subplots(figsize=(16, 8))
+fig = sns.boxplot(x=var, y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000)
+plt.xticks(rotation=90)
+
+# plt.show()
+
+#correlation matrix
+corrmat = df_train.corr(numeric_only=True)
+f, ax = plt.subplots(figsize=(12, 9))
+sns.heatmap(corrmat, vmax=.8, square=True)
+
+# plt.show()
+
+#saleprice correlation matrix
+k = 10 #number of variables for heatmap
+cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
+cm = np.corrcoef(df_train[cols].values.T)
+sns.set(font_scale=1.25)
+hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+
+# plt.show()
+
+#scatterplot
+sns.set()
+cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
+sns.pairplot(df_train[cols], size = 2.5)
+
+# plt.show()
+
+#missing data
+total = df_train.isnull().sum().sort_values(ascending=False)
+percent = (df_train.isnull().sum()/df_train.isnull().count()).sort_values(ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+print(missing_data.head(20))
+
+#dealing with missing data
+df_train = df_train.drop((missing_data[missing_data['Total'] > 1]).index, axis=1)
+df_train = df_train.drop(df_train.loc[df_train['Electrical'].isnull()].index)
+assert df_train.isnull().sum().max() == 0 #just checking that there's no missing data missing...
+
+#standardizing data
+saleprice_scaled = StandardScaler().fit_transform(df_train.to_numpy())
+low_range = saleprice_scaled[saleprice_scaled[:,0].argsort()][:10]
+high_range= saleprice_scaled[saleprice_scaled[:,0].argsort()][-10:]
+print('outer range (low) of the distribution:')
+print(low_range)
+print('\nouter range (high) of the distribution:')
+print(high_range)
